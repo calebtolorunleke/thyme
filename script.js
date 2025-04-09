9fc425f441a2ec7bd26c9af494427065f272d248
Caleb Adebayo <72946335+calebtolorunleke@users.noreply.github.com>
2025-04-09 21:40:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+// const Emmanuel = "Male"
+
+// const banana = "100"
+
+// let age = 20;
+
+// age = 25
+
+// function person(params) {
+//     const collins = "male"
+
+//     console.log(collins)
+// }
+
+// const collins = "male"
+
+// if (Emmanuel == "Male") {
+//     console.log("Emmanuel is a man")
+//     let mary = "female"
+
+//     console.log(mary)
+
+
+//     console.log(Emmanuel)
+
+//     var test = "i escape"
+// }
+
+// console.log(test)
+
+let firstName = 'john'
+
+let lastName = "obi"
+
+
+let greeting = "Hello, " + firstName + ' ' + lastName
+
+console.log(greeting)
+
+let greet = `Hello, ${firstName} ${lastName}!`
+
+let mynum = 08160964896
+
+console.log(greet)
+
+console.log(typeof (firstName))
+
+console.log(typeof (lastName))
+
+console.log(typeof (greet))
+
+console.log(typeof (mynum))
+
+console.log(firstName.length)
+
+console.log(lastName.length)
+
+console.log(greeting.length)
+
+console.log(firstName.toUpperCase())
+
+console.log(lastName.toUpperCase())
+
+console.log(greeting.toLowerCase())
+
+let text = "Hello, world!"
+
+console.log(text.includes("world!"))
+
+console.log(text.substring(0,9)) 
+
+let num = '10.678'
+
+console.log(num)
+
+let isloggedIn = true
+let admin = false
+
+if(isloggedIn){
+    console.log("Welcome!")}
+    else {
+
+    console.log("Bye")
+    }
+
+
+    let selectedColor = null
+
+    function chooseColor(color){
+        if(color){
+            selectedColor=color
+        }
+        else{
+            selectedColor = null;
+        }
+        
+    }
+
+    chooseColor("red")
+
+        console.log(selectedColor)
